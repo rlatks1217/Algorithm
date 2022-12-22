5f7187df66895b85e6b7996bf73c94bc42acce9f
KOSA <KOSA@DESKTOP-5S93LAB>
2022-12-22 11:04:26 +0900

    2022 12 21

---
+++ _backjoon/src/string/Ex10654.java
@@ -0,0 +1,24 @@
+package string;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Ex10654 {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		try {
+			int c = br.read();
+			System.out.println(c);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
