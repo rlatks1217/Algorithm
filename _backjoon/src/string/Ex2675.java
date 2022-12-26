e0358e534577cafead99b75d4b65a4b02632688e
KOSA <KOSA@DESKTOP-5S93LAB>
2022-12-26 17:40:58 +0900

    2022 12 26

---
+++ _backjoon/src/string/Ex2675.java
@@ -0,0 +1,25 @@
+package string;
+
+import java.util.Scanner;
+
+public class Ex2675 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		
+		for (int i = 0; i < T; i++) {
+			int R = sc.nextInt();
+			String[] S = sc.next().split("");
+			String result = "";
+			for (int j = 0; j < S.length; j++) {
+				for (int k = 0; k < R; k++) {
+					result += S[j];
+				}
+			}
+			System.out.println(result);
+		}
+		
+	}
+}
