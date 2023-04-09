3067b80e31967a130b18e110a138f759fcb5423e
rlatks1217 <rlatks1217@gmail.com>
2023-04-10 08:46:10 +0900

    algorithm commit

---
+++ algo/src/LV1/자리수_더하기.java
@@ -0,0 +1,25 @@
+package LV1;
+
+public class 자리수_더하기 {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println();
+		
+	}
+	
+	public static int solution(int n) {
+		
+		String[] number = (n + "").split("");
+		
+		int sum = 0;
+		
+		for (int i = 0; i < number.length; i++) {
+			sum += Integer.parseInt(number[i]);
+		}
+		
+		return sum;
+	}
+
+}
