8df81e0f325a890ae3b63fb3f35e7548a53f29c6
rlatks1217 <rlatks1217@gmail.com>
2023-04-14 18:58:30 +0900

    0414 Algorithm commit

--- algo/src/_programers/Test.java
+++ algo/src/_programers/Test.java
@@ -9,42 +9,29 @@ import java.io.IOException;
 
 public class Test {
     public static void main(String[] args) throws IOException {
-    	// 1. URL을 만들기 위한 StringBuilder.
-        StringBuilder urlBuilder = new StringBuilder("http://openapi.tour.go.kr/openapi/service/TourismResourceStatsService/getPchrgTrrsrtVisitorList"); /*URL*/
-        // 2. 오픈 API의요청 규격에 맞는 파라미터 생성, 발급받은 인증키.
-        urlBuilder.append("?" + URLEncoder.encode("ServiceKey","UTF-8") + "=인증키넣기"); /*Service Key*/
-        urlBuilder.append("&" + URLEncoder.encode("returnType","UTF-8") + "=" + URLEncoder.encode("JSON", "UTF-8")); /*XML 또는 JSON*/
-        urlBuilder.append("&" + URLEncoder.encode("numOfRows","UTF-8") + "=" + URLEncoder.encode("10", "UTF-8")); /*한 페이지 결과 수*/
-        urlBuilder.append("&" + URLEncoder.encode("pageNo","UTF-8") + "=" + URLEncoder.encode("1", "UTF-8")); /*페이지 번호*/
-        urlBuilder.append("&" + URLEncoder.encode("cond[country_nm::EQ]","UTF-8") + "=" + URLEncoder.encode("가나", "UTF-8")); /*한글 국가명*/
-        urlBuilder.append("&" + URLEncoder.encode("cond[country_iso_alp2::EQ]","UTF-8") + "=" + URLEncoder.encode("GH", "UTF-8")); /*ISO 2자리코드*/
-        // 3. URL 객체 생성.
+        StringBuilder urlBuilder = new StringBuilder("http://openapi.tour.go.kr/openapi/service/TourismAccdtService/getTourismExprncrtList"); /*URL*/
+        urlBuilder.append("?" + URLEncoder.encode("serviceKey","UTF-8") + "=DY%2BU9ZQA5S%2BiRKzmdgPat9EGmlsdgAGNcLa13GPjOTzMlfHhhZ7v3u3Glhpd%2FWVDZooQzdFCNSmWw1U4UrmUWA%3D%3D"); /*Service Key*/
+        urlBuilder.append("&" + URLEncoder.encode("YY","UTF-8") + "=" + URLEncoder.encode("2011", "UTF-8")); /*년도*/
+        urlBuilder.append("&" + URLEncoder.encode("_type","UTF-8") + "=" + URLEncoder.encode("json", "UTF-8")); /*데이터 형식*/
         URL url = new URL(urlBuilder.toString());
-        // 4. 요청하고자 하는 URL과 통신하기 위한 Connection 객체 생성.
         HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        // 5. 통신을 위한 메소드 SET.
         conn.setRequestMethod("GET");
-        // 6. 통신을 위한 Content-type SET. 
         conn.setRequestProperty("Content-type", "application/json");
-        // 7. 통신 응답 코드 확인.
         System.out.println("Response code: " + conn.getResponseCode());
-        // 8. 전달받은 데이터를 BufferedReader 객체로 저장.
         BufferedReader rd;
         if(conn.getResponseCode() >= 200 && conn.getResponseCode() <= 300) {
             rd = new BufferedReader(new InputStreamReader(conn.getInputStream()));
         } else {
             rd = new BufferedReader(new InputStreamReader(conn.getErrorStream()));
         }
-        // 9. 저장된 데이터를 라인별로 읽어 StringBuilder 객체로 저장.
         StringBuilder sb = new StringBuilder();
         String line;
         while ((line = rd.readLine()) != null) {
             sb.append(line);
         }
-        // 10. 객체 해제.
         rd.close();
         conn.disconnect();
-        // 11. 전달받은 데이터 확인.
         System.out.println(sb.toString());
     }
+
 }
