58051614361a87fbaa9be25444bc2950a5871cd1
rlatks1217 <rlatks1217@gmail.com>
2023-05-03 09:40:32 +0900

    0503 commit

--- algo/src/_programers/기사단원의무기.java
+++ algo/src/_programers/기사단원의무기.java
@@ -14,7 +14,7 @@ public class 기사단원의무기 {
 		for (int i = 1; i <= number; i++) {
 			int count = 0;
 			for (int j = 1; j * j <= i; j++) {
-				if (j * j == 0) {
+				if (j * j == i) {
 					count++;
 				} else if (i % j == 0) {
 					count += 2;
