63a2c8b2d0213a98a328dd5b8610deef7c0b9a29
rlatks1217 <rlatks1217@gmail.com>
2023-06-06 00:27:15 +0900

    0606 Algorithm commit

---
+++ algo/src/string/Ex27866.java
@@ -0,0 +1,17 @@
+package string;
+
+import java.util.Scanner;
+
+public class Ex27866 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		int i = sc.nextInt();
+		
+		System.out.println(s.charAt(i-1));
+		
+		sc.close();
+	}
+
+}
