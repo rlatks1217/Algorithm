7b082dd262fbeaeff5b3c63a468010a62ef7fe0d
rlatks1217 <rlatks1217@gmail.com>
2023-06-19 00:08:17 +0900

    0619 Algorithm commit

---
+++ algo/src/입출력과사칙연산/Ex10171.java
@@ -0,0 +1,13 @@
+package 입출력과사칙연산;
+
+public class Ex10171 {
+
+	public static void main(String[] args) {
+		 
+		 System.out.println("\\    /\\");
+		 System.out.println(" )  ( ')");
+		 System.out.println("(  /  )");
+		 System.out.println(" \\(__)|");
+	}
+
+}
---
+++ algo/src/입출력과사칙연산/Ex10172.java
@@ -0,0 +1,13 @@
+package 입출력과사칙연산;
+
+public class Ex10172 {
+
+	public static void main(String[] args) {
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+	}
+//\앞에 나온 글자가 문자열임을 나타내는 기호임
+}
