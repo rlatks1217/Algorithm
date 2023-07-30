cf7bd327c3c3aa62192f31484ceb39913df935d9
rlatks1217 <rlatks1217@gmail.com>
2023-07-31 08:17:34 +0900

    0731 Algorithm commit

---
+++ algo/src/시간복잡도/Ex24262.java
@@ -0,0 +1,19 @@
+package 시간복잡도;
+import java.util.Scanner;
+
+public class Ex24262 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		sc.nextInt();
+		
+		System.out.println(1);
+		System.out.println(0);
+		
+		sc.close();
+	}
+
+}
+//어떤 수가 입력으로 들어오던 실행횟수는 1임 즉, 상수라고 할 수 있음 -> 상수항의 차수는 0
