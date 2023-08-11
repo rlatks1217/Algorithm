312691b88c8b4bed81531626013d3e2f04690cbf
unknown <kim san>
2023-08-11 18:28:29 +0900

    0811 Algorithm commit

---
+++ algo/src/string/Ex10798.java
@@ -0,0 +1,33 @@
+package string;
+
+import java.util.Scanner;
+
+public class Ex10798 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String[] arr = new String[15];
+		
+		
+		for (int i = 0; i < 5; i++) {
+			String a = sc.nextLine();
+			for (int j = 0; j < a.length(); j++) {
+				arr[j] += a.charAt(j) + "";
+			}
+		}
+		String result = "";
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] == null) {
+				break;
+			}
+			result += arr[i];
+		}
+		
+		result = result.replace("null", "");
+		
+		System.out.println(result);
+		
+		sc.close();
+	}
+}
