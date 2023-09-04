b71a4444d49ab244c3fcb68be8e6ee8dbb30db2c
Kim san <rlatks1217@gmail.com>
2023-09-04 15:24:10 +0900

    0904 Algorithm commit

--- algo/src/약수_배수와소수2/Ex17103.java
+++ algo/src/약수_배수와소수2/Ex17103.java
@@ -1,42 +1,35 @@
 package 약수_배수와소수2;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Scanner;
 
 public class Ex17103 {
-	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int T = Integer.parseInt(br.readLine());
-		
-		int[] numbers = new int[T];
-		for (int i = 0; i < T; i++) {
-			numbers[i] = Integer.parseInt(br.readLine());
-		}
-		
-		Arrays.sort(numbers);
-		
-		boolean[] checkPrime = new boolean[numbers[numbers.length-1]+1];
-		for (int i = 2; i <= Math.sqrt(numbers[numbers.length-1]); i++) {
-			if (checkPrime[i]) {
-				continue;
-			}
-			
-			for (int j = i+i; j <= numbers[numbers.length-1]; j += i) {
-				checkPrime[j] = true;
+	static boolean num[];
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		num = new boolean[1000001];
+		num[0] = num[1] = true;
+		for (int i = 2; i * i <= 1000000; i++) {
+			if (!num[i]) {
+				for (int j = i + i; j <= 1000000; j += i) {
+					num[j] = true;
+				}
 			}
 		}
-		
-		for (int i = 0; i < numbers.length; i++) {
-			for (int j = 2; j < numbers[i]; j++) {
-				
+		int T = sc.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			int temp = (tc+1)*2;
+			int ans = 0;
+
+			for (int j = 2; j <= temp / 2; j++) {
+				if (!num[j] && !num[temp - j])
+					ans++;
 			}
+
+			System.out.print(ans);
 		}
 	}
+
 }
+//중괄호 없이 if문을 사용할 경우 if문 바로 밑에 한줄만 인식함 => 그 밑줄부터는 if문 블럭 밖에 있는 것으로 인식함
