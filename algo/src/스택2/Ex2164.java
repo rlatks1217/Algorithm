4d1a5f7631649b31e13a37f4e12116186e5f15f9
rlatks1217 <rlatks1217@gmail.com>
2023-09-30 22:45:12 +0900

    test

---
+++ algo/src/스택2/Ex2164.java
@@ -0,0 +1,16 @@
+package 스택2;
+
+public class Ex2164 {
+	
+	static int b = 2;
+	
+	static {
+		System.out.println(b);
+		
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("시작");
+		
+	}
+}
