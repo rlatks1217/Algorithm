85f3bd73e2cb8ef469ad4fddcd0db7d544507a15
San Kim <rlatks1217@gmail.com>
2024-02-13 06:49:47 +0900

    0213 알고리즘 문제 풀이

---
+++ algo/src/스택_큐_덱/Ex24511.java
@@ -0,0 +1,45 @@
+package 스택_큐_덱;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Ex24511 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        Deque<Integer> queue = new ArrayDeque<>();
+        int n = Integer.parseInt(br.readLine());
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int[] arr = new int[n];
+        for(int i = 0; i < n;i++){
+            arr[i] = Integer.valueOf(st.nextToken());
+        }
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < n; i++){
+            int tmp = Integer.parseInt(st.nextToken());
+            if(arr[i] == 0){
+                queue.offer(tmp);
+            } 
+        }
+        
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < m; i++){
+            int tmp = Integer.parseInt(st.nextToken());
+            queue.offerFirst(tmp);
+            sb.append(queue.pollLast() + " ");
+        }
+        System.out.println(sb.toString());
+		
+	}
+
+}
