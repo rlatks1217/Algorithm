421a2c9851a654cfd3ddae2a1a57ab6c258fd92a
San Kim <rlatks1217@gmail.com>
2024-07-31 07:44:17 +0900

    0731 Algorithm commit

---
+++ algo/src/Doit/Ex11660.java
@@ -0,0 +1,43 @@
+package Doit;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+public class Ex11660 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		
+		int[][] s = new int[n + 1][n + 1];
+		
+		for (int i = 1; i <= n; i++) {
+			st = new StringTokenizer(br.readLine());
+			for (int j = 1; j <= n; j++) {
+				s[i][j] = Integer.parseInt(st.nextToken()) + s[i][j - 1] + s[i - 1][j] - s[i- 1][j - 1]; 
+			}
+		}
+		
+		for (int i = 0; i < m; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x1 = Integer.parseInt(st.nextToken());
+			int y1 = Integer.parseInt(st.nextToken());
+			int x2 = Integer.parseInt(st.nextToken());
+			int y2 = Integer.parseInt(st.nextToken());
+			
+			int sum = s[x2][y2] - s[x1 - 1][y2] - s[x2][y1 - 1] + s[x1 - 1][y1 - 1];
+			sb.append(sum + "\n");
+		}
+		System.out.println(sb);
+		br.close();
+	}
+
+}
+// x는 몇번째 행인지를 의미 / y는 몇번째 열인지를 의미
